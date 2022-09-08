483dc94ea7675f38d5bf3c1b0a57e92b8261f18c
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-08 15:32:43 +0000

    Created sidebar, topbar, and beginning of options inputs

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const getValuePosition = function() {
+  let valuePos = document.getElementById("valuePosition").value;
+  return valuePos
+}
+
+const pullOptions = function() {
+  console.log(getValuePosition());
+}
