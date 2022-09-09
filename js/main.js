929a75cff79fa3e246b49bf7cae758754507ecd3
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-09 00:41:59 +0000

    bug checking input boxes, added bar customization input boxes

--- js/main.js
+++ js/main.js
@@ -8,6 +8,25 @@ const getCategories = function() {
   return cat;
 };
 
+// Function to get category values
+const getCategoryValues = function() {
+  let categoryValues = document.getElementById("categoryValues").value.split(", ");
+  for (let i = 0; i < categoryValues.length; i++) {
+    categoryValues[i] = Number(categoryValues[i]);
+  }
+  return categoryValues;
+}
+
+// Function to get values of scale
+const getScaleValue = function() {
+  return Number(document.getElementById("scaleValue").value);
+}
+
+// Function to get scale by value, less than max scale value
+const getScaleByValue = function() {
+  return Number(document.getElementById("scaleByValue").value);
+}
+
 // Function to get value position from form
 const getValuePosition = function() {
   return document.getElementById("valuePosition").value;
@@ -18,18 +37,21 @@ const pullOptions = function() {
   data = [];
   // Pushes pulled form data into master data array
   data.push(getCategories());
+  data.push(getCategoryValues());
+  data.push(getScaleValue());
+  data.push(getScaleByValue());
   data.push(getValuePosition());
   console.log(data); // Test output
 };
 
 // Function to check if data was filled out
 const filledOut = function() {
-  let inputs = document.getElementsByClassName("userInput");
+  let inputs = document.getElementsByClassName("mandatoryInput");
   let complete = true;
   for (let i = 0; i < inputs.length; i++) { // Loop through form to see if any inputs are blank
     if (inputs[i].value === "") { // If any input is incomplete, visually show
       inputs[i].classList.add("incomplete"); // Add class with animation
-      setInterval(function() {inputs[i].classList.remove("incomplete")}, 5000); // Remove class with animation
+      setInterval(function() {inputs[i].classList.remove("incomplete")}, 3000); // Remove class with animation
       complete = false;
     }
   }
