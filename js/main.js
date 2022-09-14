d39fffc0a3876147fd45ed26b4ee37d9e6da5733
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-09-14 03:48:32 +0000

    Implemented multiple failsafes as well as completed and finalized drawing of bars for the chart

--- js/main.js
+++ js/main.js
@@ -6,18 +6,26 @@ let categoryBarColour = {}; // Object containing colours for each bar associated
 const getCategories = function() {
   let cat = {};
   cat.mainCatOne = $("#mainCategoryOne").val();
-  cat.subCatOne = $("#subCategoryOne").val().split(", ");
+
+  // Pull from each input box under the sub category class
+  cat["subCatOne"] = [];
+  $(".barCategoryName").each(function(){
+    cat["subCatOne"].push($( this ).val());
+  });
+  cat["subCatOne"] = cat["subCatOne"].filter(e => e); // Filter out empty values
+
   cat.mainCatTwo = $("#mainCategoryTwo").val();
-  cat.subCatTwo = $("#subCategoryTwo").val().split(", ");
   return cat;
 };
 
 // Function to get category values
 const getCategoryValues = function() {
-  let categoryValues = $("#categoryValues").val().split(", ");
-  for (let i = 0; i < categoryValues.length; i++) {
-    categoryValues[i] = Number(categoryValues[i]);
-  }
+  let categoryValues = [];
+  $(".barCategoryValue").each(function() {
+    categoryValues.push($( this ).val());
+  });
+  categoryValues = categoryValues.filter(Number); // Filter out empty values
+
   return categoryValues;
 };
 
@@ -41,6 +49,7 @@ const getBarWidth = function() {
   return Number($("#barWidth").val());
 };
 
+/*
 // Function to get height of bars
 const getBarHeight = function() {
   return Number($("#barHeight").val());
@@ -50,6 +59,7 @@ const getBarHeight = function() {
 const getBarSpacing = function() {
   return Number($("#barSpacing").val());
 };
+*/
 
 // Function to create pickers for colour of bars
 const createBarColourPicker = function() {
@@ -58,7 +68,6 @@ const createBarColourPicker = function() {
 
   // Clear out previously added colour pickers to form for fresh start
   $(".colourPickers").remove();
-
   // Define and append header for pickers
   form.append("<h5 class='colourPickers'>Bar Colours:</h5>");
 
@@ -66,23 +75,26 @@ const createBarColourPicker = function() {
   for (let i = 0; i < cats.length; i++) {
     // Define and append labels for each picker
     form.append("<label for='" + cats[i] + "' class='colourPickers'>" + cats[i] + ":" + "</label>");
-
     // Define and append pickers
-    form.append("<input id='" + cats[i] + "' class='colourPickers' type='color' value='#3E89B8'>");
+    if (categoryBarColour[cats[i]] === undefined) {
+      form.append("<input id='" + cats[i] + "' class='colourPickers' type='color' value='#3E89B8'>");
+    } else {
+      form.append("<input id='" + cats[i] + "' class='colourPickers' type='color' value='" + categoryBarColour[cats[i]] + "'>");
+    }
   }
-
   // Define and append button for submitting picked colours
   form.append("<button id='submitColours' class='colourPickers' type='button' onclick='getBarColour()'>Submit Colours</button>");
 };
 
 // Function to get colour of bars
 const getBarColour = function() {
-  categoryBarColour = {};
   let cats = options[0].subCatOne;
-
   // Pushed inputted colours of bars into global object
   for (let i = 0; i < cats.length; i++) {
     categoryBarColour[cats[i]] = $("#" + cats[i]).val();
+
+    // Update styling of bars in chart when new colour is picked and submitted
+    $("#" + cats[i] + "Bar").css("background-color", categoryBarColour[cats[i]]);
   }
 
   console.log(categoryBarColour); // Test output
@@ -98,6 +110,15 @@ const getFontColour = function() {
   return $("#fontColour").val();
 };
 
+// Function to add an input box for an additional category and value to be inputted
+const addCategory = function() {
+  const prevIdOne = $("#addCategoryButton").prevAll("input").length - 2;
+  const prevIdTwo = $("#scaleValueHeader").prevAll("input").length - (prevIdOne + 2);
+
+  $("#addCategoryButton").before("<input type='text' class='barCategoryName' placeholder='Red...' value=''>");
+  $("#scaleValueHeader").before("<input type='number' class='barCategoryValue' placeholder='" + (prevIdTwo + 1).toString() + "...' value=''>");
+}
+
 // Function to pull data
 const pullOptions = function() {
   options = [];
@@ -111,8 +132,10 @@ const pullOptions = function() {
   options.push(getScaleByValue());
   options.push(getValuePosition());
   options.push(getBarWidth());
+  /*
   options.push(getBarHeight());
   options.push(getBarSpacing());
+  */
   options.push(getFontSize());
   options.push(getFontColour());
 
@@ -128,35 +151,116 @@ const filledOut = function() {
 
   $(".mandatoryInput").each(function() {
     if ($( this ).val() === "") { // Check if a mandatory input field is empty
-      // Notify visually which field needs completing
-      $( this ).css("border-color", "red");
-      $( this ).fadeOut(500);
-      $( this ).fadeIn(500);
-
+      pulse(this);
       complete = false;
     }
   });
 
+  // Temporarily get inputs to test if the number of each matches
+  let tempCatNames = [];
+  $(".barCategoryName").each(function() {
+    if ($( this ).val() !== "") {
+      tempCatNames.push($( this ).val());
+    }
+  });
+  let tempCatValues = [];
+  $(".barCategoryValue").each(function() {
+    if ($( this ).val() !== "") {
+      tempCatValues.push($( this ).val());
+    }
+  });
+  // Check if the number of categories and number of values for those categories are equal
+  if (tempCatNames.length !== tempCatValues.length) {
+    pulse("#subCategoryHeader");
+    pulse("#categoryValueHeader");
+    alert("Ensure the number of values matches the number of categories!");
+    complete = false;
+  }
+
+  $(".mustBeNum").each(function() {
+    if ($( this ).val() === "") { // Check if pulled inputs aren't numbers
+      pulse(this);
+      alert("Ensure these values are numbers!");
+      complete = false;
+    }
+  });
+
+  // Check if the scale value is divisible by the scale-by value
+  if ($("#scaleValue").val() % $("#scaleByValue").val() !== 0) {
+    pulse("#scaleValue");
+    pulse("#scaleByValue");
+    alert("Ensure the scale is divisible by the scale-by value!");
+    complete = false;
+  }
+
+  // Check if the bar width value doesn't exceed the side of the x-axis
+  if ($("#barWidth").val() > Math.round((((Number($("#xAxis").css("width").replace("px", ""))) / tempCatValues.length) - 25))) {
+    pulse("#barWidth");
+    alert("Ensure the bar width isn't larger than " + (Math.round((((Number($("#xAxis").css("width").replace("px", ""))) / tempCatValues.length) - 25))).toString() + "px!");
+    complete = false;
+  }
+
   return complete; // Return if form is complete
 };
 
+// Function to pulse incomplete or incorrect inputs
+const pulse = function(ele) {
+  // Notify visually which field needs completing or modifying
+  $(ele).css("border-color", "red");
+  $(ele).css("color", "red");
+  $(ele).fadeOut(500);
+  $(ele).fadeIn(500);
+}
+
 // Function to add elements in html to show drawn and generated bar chart
 const createChart = function() {
-  // Declarations to pull in variables from css stylesheet
-  const root = document.querySelector(':root');
-  const rootStyles = getComputedStyle(root);
+  const bars = $(".bars");
+  const barSpacingX = (100 * (1 / data[0].length)); // How far apart bars should be based on number of categories
+  let offsetPercentY;
+  let offsetPercentX = 5;
+
+  $(".createdBars").remove(); // Remove any previously created and appended bars
+
+  // Loop through each category to create and style bars for said category
+  for (let i = 0; i < data[0].length; i++) {
+    offsetPercentY = 100 * (data[0][i] / options[1]);
+    let tempColour;
+    // Check if the colour of the category has already been picked
+    if (categoryBarColour[options[0].subCatOne[i]] === undefined) {
+      tempColour = "#3E89B8";
+    } else {
+      tempColour = categoryBarColour[options[0].subCatOne[i]];
+    }
+
+    // Append html container to pages
+    bars.append("<div id='" + options[0].subCatOne[i] + "Bar' class='createdBars'></div>");
+    // Style appended html container
+    $("#" + options[0].subCatOne[i] + "Bar").css({
+      "position": "absolute",
+      "bottom": "0",
+      "left": offsetPercentX + "%",
+      "height": offsetPercentY + "%",
+      "width": options[4] + "px",
+      "background-color": tempColour
+    });
+    offsetPercentX += barSpacingX; // Increment x axis offset for bar styling
+  }
 };
 
 // Function to call functions to check form and pull from it
 const checkForm = function() {
-
-  $(".mandatoryInput").css("border-color", ""); // Clear any previously visual reminders of incomplete input fields
+  // Clear any previously visual reminders of incomplete input fields
+  $(".mandatoryInput").css("border-color", "");
+  $(".sidebar h5").css("color", "peachpuff");
+  $("#options input").css({
+    "color": "black",
+    "border-color": ""
+  });
 
   if (filledOut()) {
     pullOptions(); // If the form is completed, pull data from inputs
+    createChart();
   } else {
     console.log("Incomplete Form");
   }
-
-  createChart();
 };
